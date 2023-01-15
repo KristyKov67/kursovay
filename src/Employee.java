9fcf2f2c7c489efc99a0ec32bfedfb25bd16ceb7
Kristy <krisstyle@inbox.ru>
2023-01-15 20:14:11 +0300

    1

---
+++ src/Employee.java
@@ -0,0 +1,49 @@
+public class Employee {
+    private String fullName;
+    private int otdel;
+    private float salary;
+    private int id;
+    private static int counter = 1;
+
+
+    public Employee(String fullName, int otdel, float salary) {
+        this.fullName = fullName;
+        this.otdel = otdel;
+        this.salary = salary;
+        this.id = counter++;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+
+    public int getOtdel() {
+        return otdel;
+    }
+
+    public void setOtdel(int otdel) {
+        this.otdel = otdel;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "fullName='" + fullName + '\'' +
+                ", otdel=" + otdel +
+                ", salary=" + salary +
+                '}';
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+    private static final Employee[] employees = {
+            new Employee("Петров", 2, 18000),
+            new Employee("Сергеев", 1, 10000)
+    };
+
+
+    private static void printSotrudnik() {
+        for (Employee employee : employees) {
+            System.out.println(employee);
+        }
+
+        }
+
     }
 }
+
